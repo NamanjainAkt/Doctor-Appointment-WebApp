43d4b9486a310c57f27e7ff6c8f6cf8f198a1340
Naman jain <namanjainakt007@gmail.com>
2025-03-04 04:50:34 +0530

    added some basic structures and styles to the app

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
-const Navbar = () => {history
+const Navbar = () => {
+  const navigate=useNavigate();
+  const [showMenu,setShowMenu]=useState(false);
+  const [token,setToken]=useState(true);
   return (
     <div className='flex justify-between items-center py-4 test-sm mb-5 border-b border-b-gray-400'>
       <img src={assets.logo} className='w-44 cursor-pointer' />
@@ -24,8 +29,21 @@ const Navbar = () => {history
           <hr className='border-none outline-none h-0.5 bg-[#5f6fff] w-3/5 m-auto hidden'/>
         </NavLink>
       </ul>
-      <div>
-        <button>Create Account</button>
+      <div className='flex items-center gap-4'>
+        {token
+        ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+          <img className='w-8 rounded-full ' src={assets.profile_pic} alt="" />
+          <img className='w-2.5 group-hover:rotate-180 transition-all duration-300' src={assets.dropdown_icon} alt="" />
+          <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+              <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4 '>
+                <p className='cursor-pointer hover:text-black'>My Profile</p>
+                <p className='cursor-pointer hover:text-black'>My Appointments</p>
+                <p className='cursor-pointer hover:text-black'>Logout</p>
+              </div>
+          </div>
+        </div>
+        :<button onClick={()=>navigate('/login')} className='bg-[#5f6fff] text-white px-8 py-2 rounded-full font-light hidden md:block cursor-pointer'>Create Account</button>
+      }
       </div>
     </div>
   )
