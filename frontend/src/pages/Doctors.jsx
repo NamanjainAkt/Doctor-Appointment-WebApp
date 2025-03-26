7fbfbbd7e40a27f09c65d8b72be6094759b32946
Naman jain <namanjainakt007@gmail.com>
2025-03-27 02:25:06 +0530

    frontend part of the app is done

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -9,6 +9,7 @@ const Doctors = () => {
   const [filterDoc, setFilterDoc] = useState([])
   const { doctors } = useContext(AppContext)
   const navigate = useNavigate()
+  const [showFilter, setShowFilter] = useState(false)
   
   const applyFilter = useCallback(() => {
     if (speciality) {
@@ -29,7 +30,8 @@ const Doctors = () => {
     <div>
       <p className='text-gray-600 '>Browse Through the Dooctors Specialist.</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-[#5f6fff] text-white' : ""}`} onClick={()=>setShowFilter(prev=> !prev)}>Filters</button>
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? "flex" : "hidden sm:flex"}`}>
           <p onClick={() => speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General physician" ? "bg-indigo-100 text-black" : ""}`}>General Physician</p>
           <p onClick={() => speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>Gynecologist</p>
           <p onClick={() => speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>Dermatologist</p>
